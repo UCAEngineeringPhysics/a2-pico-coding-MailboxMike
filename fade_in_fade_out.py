f069135a232cf63a2e2cb82980c49a12f9a963fa
MailboxMike <501barbereric@gmail.com>
2025-09-09 12:30:39 -0500

    Update fade_in_fade_out.py

--- fade_in_fade_out.py
+++ fade_in_fade_out.py
@@ -1,7 +1,17 @@
 from machine import Pin, PWM, Timer
 from time import sleep
 
-
 # SETUP
+dimmer = PWM(Pin(15))
+dimmer.freq(1000)
 
-# LOOP
+# 
+while True:
+    for duty in range(65536):
+        dimmer.duty_u16(duty)
+        sleep(.0001)
+    for duty in range(65536,0,-1):
+        dimmer.duty_u16(duty)
+        sleep(.0001)
+
+LOOP
